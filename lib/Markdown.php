07a371bed53d542816354f1ae87179502468fadd
Chris Cornutt <enygma@phpdeveloper.org>
2012-01-10 18:42:10 -0600

    adding anchor tag support to the Markdown class, adding the 5th slide from the demo presentation

--- lib/Markdown.php
+++ lib/Markdown.php
@@ -8,6 +8,13 @@ namespace Lib;
 
 class Markdown
 {
+    /**
+     * Parse the markdown syntax
+     *
+     * @param string $contents Markdown file contents
+     *
+     * @return string $contents Formatted results
+     */
     public function parse($contents)
     {
 
@@ -22,25 +29,83 @@ class Markdown
         return $contents;
     }
 
+    /**
+     * Format markdown syntax for bold text
+     * 
+     * @param string $contents Contents of the markdown file
+     * 
+     * @return string $contents Formatted contents
+     */
     private function _formatBold($contents)
     {
         return preg_replace('/(\*|#){2}(.+?)(\*|#){2}/ms', '<strong>$2</strong>', $contents);
     }
+
+    /**
+     * Format markdown syntax for italicized text
+     * 
+     * @param string $contents Contents of the markdown file
+     * 
+     * @return string $contents Formatted contents
+     */
     private function _formatItalic($contents)
     {
         return preg_replace('/(\*|#){1}(.+?)(\*|#){1}/ms', '<i>$2</i>', $contents);
     }
+
+    /**
+     * Format markdown syntax for header, lvl1 tags (h1)
+     * 
+     * @param string $contents Contents of the markdown file
+     * 
+     * @return string $contents Formatted contents
+     */
     private function _formatHeadline1($contents)
     {
         return preg_replace('/(.+?)\\n(=+)\\n/ms','<h1>$1</h1>',$contents);
     }
+
+    /**
+     * Format markdown syntax for header, lvl2 tags (h2)
+     * 
+     * @param string $contents Contents of the markdown file
+     * 
+     * @return string $contents Formatted contents
+     */
     private function _formatHeadline2($contents)
     {
         return preg_replace('/(.+?)\\n(\-+)\\n/ms','<h2>$1</h2>',$contents);
     }
+
+    /**
+     * Format markdown syntax for paragraph tags
+     * 
+     * @param string $contents Contents of the markdown file
+     * 
+     * @return string $contents Formatted contents
+     */
     private function _formatParagraph($contents)
     {
         return preg_replace('/\\n\\n(.+?)\\n/','<p>$1</p>',$contents);
+    }
+
+    /**
+     * Format markdown syntax and replace with anchor tags
+     * match: [prezi.com](http://prezi.com "prezi.com")
+     * 
+     * @param string $contents Contents of the markdown file
+     * 
+     * @return string $contents Formatted contents
+     */
+    private function _formatAnchor($contents)
+    {
+        preg_match('/\[(.+?\]\((.+?) "(.+?)"\))/',$contents,$match);
+
+        if (!empty($match[0])) {
+            $contents = str_replace($match[0],'<a href="'.$match[2].'">'.$match[3].'</a>',$contents);    
+        }
+
+        return $contents;
     }
 }
 
---
+++ sample-presentation/slide5.md
@@ -0,0 +1,4 @@
+<!--- id=its,class=step,data-x=850,data-y=3000,data-rotate=90,data-scale=5 --->
+<p>It's a <strong>presentation tool</strong> <br/>
+inspired by the idea behind [prezi.com](http://prezi.com "prezi.com")<br/>
+and based on the <strong>power of CSS3 transforms and transitions</strong> in modern browsers.</p>
